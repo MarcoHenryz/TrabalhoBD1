7e81a59300030d1f907282df8d863985155d5b6f
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2025-11-10 20:06:39 -0300

    feat(back): setup básico com spring initializr

---
+++ src/back/src/main/java/com/uel/api_notaki/ApiNotakiApplication.java
@@ -0,0 +1,13 @@
+package com.uel.api_notaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNotakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNotakiApplication.class, args);
+	}
+
+}
---
+++ src/back/src/test/java/com/uel/api_notaki/ApiNotakiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uel.api_notaki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNotakiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
